1ed211094b13051a4b90ba84702d0d16c0e8bc04
Vishnupriya <priya.singh9349@gmail.com>
2023-11-30 23:23:10 +0530

    Added code for Valid First Name

--- regex-junit/src/main/java/com/day11/Main.java
+++ regex-junit/src/main/java/com/day11/Main.java
@@ -1,7 +1,40 @@
 package com.day11;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static boolean IsValidFirstName(String firstName) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        return firstName.matches(regex);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Regex Program.");
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = "";
+        String lastName = "";
+        String email = "";
+        String phone = "";
+        String password = "";
+
+        boolean stayMain = true;
+        boolean staySub = true;
+        while (stayMain) {
+            System.out.print("Enter first Name: ");
+            firstName = scanner.next();
+            do {
+                if (!IsValidFirstName(firstName)) {
+                    System.out.print("Invalid re-enter first Name: ");
+                    firstName = scanner.next();
+                }
+            } while (!IsValidFirstName(firstName));
+
+            stayMain = false;
+        }
+
+        System.out.println("Final info ->\n First Name: " + firstName);
+
+        scanner.close();
     }
 }
